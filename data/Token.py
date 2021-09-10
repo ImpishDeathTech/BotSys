f729fe7dc1c0f0a28bd49e54d16c6b8174e3f8fc
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2021-09-10 14:37:05 -0400

    Create Token.py

---
+++ data/Token.py
@@ -0,0 +1,28 @@
+"""
+# Token.py
+# 
+# Copyright (C) 2021 Christopher Stephen Rafuse <ImpishDeathTech@protonmail.ch>
+# BSD-3-Clause
+#
+"""
+
+import json
+
+# Token is a class that loads a token from a known set contained in a json file
+# it contains one function "get()" in witch @param token_idx is set to 0 by default 
+# You should only keep one in there at a time, unless this is for a devbot
+
+class Token:
+    
+    @classmethod
+    def get(self, token_idx=0):
+        data = {}
+        
+        with open("data/Token.json", 'r') as f:
+            data = json.load(f)
+            f.close
+        
+        token = data["BOT TOKENS"][token_idx]
+        
+        if not token == None:
+            return token
