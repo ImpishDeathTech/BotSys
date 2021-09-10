9c52988be3a232725d2c3369750b13678831c642
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2021-09-10 14:32:16 -0400

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python3
+
+"""
+# Test.py
+# 
+# Copyright (C) 2021 Christopher Stephen Rafuse <ImpishDeathTech@protonmail.ch>
+# BSD-3-Clause
+#
+"""
+
+import discord, logging, sys, time
+
+from discord.ext import commands
+
+# import BotSys and C for load extensions() and TIME_STR defined in botsys-config.json 
+from cog.BotSys import BotSys, C, Token
+
+Bot = commands.Bot(command_prefix='bot.', case_insensitive=True, description="Penis")
+
+if __name__ == "__main__":
+    logging.basicConfig(filename="./log/test.log", level=logging.INFO)
+    
+    logging.info(f"BotDaemon - Loading Installed Extentions... - {time.strftime(C.TIME_STR, time.localtime(time.time()))}")
+    BotSys.load_extensions(bot=Bot,directory="cog")
+    
+    logging.info(f"BotDaemon - Running Bot... - {time.strftime(C.TIME_STR, time.localtime(time.time()))}")
+    Bot.run(Token.get(2))
+    
+    logging.info(f"BotDaemon - Exiting with code(0) - {time.strftime(C.TIME_STR, time.localtime(time.time()))}")
+    sys.exit(0) 
