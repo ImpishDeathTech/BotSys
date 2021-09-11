c6e781caeced037ef7081b638494d97886e11288
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2021-09-11 12:18:42 -0400

    Added BotSys.run() and ERR_STR

--- cog/BotSys.py
+++ cog/BotSys.py
@@ -7,12 +7,13 @@
 # BSD-3-Clause
 """
 
-
 import discord, sys, subprocess, time, logging, os, json
 
 from discord.ext import commands
+from discord.errors import LoginFailure
 from data.Token import Token
 
+# structure representing the botsys config file you load
 class BotSysConfig:
     MODERATOR_ROLE = ""
     TIME_STR = ""
@@ -20,11 +21,11 @@ class BotSysConfig:
     BOT_SCRIPT = ""
     
     LOG_STR = []
-    
     CTX_STR = []
+    ERR_STR = ""
     
-    def __init__(self):
-        with open("./data/botsys-config.json", 'r') as f:
+    def __init__(self, file_path):
+        with open(file_path, 'r') as f:
             data = json.load(f)
             
             self.MODERATOR_ROLE = data["MODERATOR ROLE"]
@@ -38,9 +39,12 @@ class BotSysConfig:
             for n in range(len(data["CTX STR"])-1):
                 self.CTX_STR.append(data["CTX STR"][n])
             
+            self.ERR_STR = data["ERR STR"]
+            
             f.close()
 
-C = BotSysConfig()
+# runtime object for configuration set to botsysp-config.json
+C = BotSysConfig(file_path="./data/botsys-config.json")
 
 
 class BotSys(commands.Cog):
@@ -56,23 +60,58 @@ class BotSys(commands.Cog):
     
     # returns the localtime of the server your bot is 
     # running on
-    
     @classmethod
     async def get_localtime(self):
         return time.strftime(C.TIME_STR, time.localtime(time.time()))
     
     
+    # load_extensions() - command classmethod for loading all 
+    # of the extensions in a directory to a bot. 
+    # To be used in the startup script
+    #
+    # @param "bot" - the bot to load extensions to
+    # @param "directory" - the directory from which to
+    # load the extensions
+    @classmethod
+    def load_extensions(self, bot, directory):
+        for f in os.listdir(directory):
+            if f.endswith('.py'):
+                f1 = f"{directory}.{f[:-3]}"
+                
+                logging.info(C.LOG_STR[12].format(f1, time.strftime(C.TIME_STR, time.localtime(time.time()))))
+                bot.load_extension(f1)
+    
+    # classmethod for running the bot using a hidden token list
+    # @param "bot" is the bot client
+    # @param "token_idx" is the numeric index of the token
+    @classmethod
+    def run(self, bot, token_idx=0):
+        lt_ = lambda:time.strftime(C.TIME_STR, time.localtime(time.time()))
+        
+        try:
+            logging.info(C.LOG_STR[10].format(lt_()))
+            bot.run(Token.get(idx=token_idx))
+            logging.info(C.LOG_STR[11].format(0, lt_()))
+            return 0
+        
+        except LoginFailure:
+            logging.error(C.ERR_STR.format("LoginFailure", "No Token Set!", lt_()))
+            logging.error(C.LOG_STR[11].format(254, lt_()))
+            return 254
+    
+    
     # classmethod for getting json data
     # @param "file_path" is what it's name suggests'
     @classmethod
-    def json_get(self, file_path):
+    async def json_get(self, file_path):
         data = {}
         
         with open(file_path, 'r') as f:
             data = json.load(f)
             f.close()
         
-        return data()
+        return data
+    
     
     # classmethod for wiriting json data
     # @param "file_path" - sets the filepath of the json to be written to
@@ -80,30 +119,15 @@ class BotSys(commands.Cog):
     # @param "indent" - sets the indent width of the file, set to 4 by default
     #                 - set to None for obfuscated json
     @classmethod
-    def json_set(self, file_path, data, indent=4):
+    async def json_set(self, file_path, data={}, indent=4):
         with open(file_path, 'w') as f:
-            json.dump(data, f, indentation=indent)
-    
-    
-    # load_extensions() - command function for loading all 
-    # of the extensions in a directory to a bot. 
-    # To be used in the startup script
-    #
-    # @param "bot" - the bot to load extensions to
-    # @param "directory" - the directory from which to
-    # load the extensions
-    
-    @classmethod
-    def load_extensions(self, bot, directory):
-        for f in os.listdir(directory):
-            if f.endswith('.py'):
-                bot.load_extension(f"{directory}.{f[:-3]}")
+            json.dump(data, f, indent=indent)
     
     ##########
     # events #
     ##########
     
-    # typical on_ready event listener
+    # typical "on_ready()" listener with logging
     @commands.Cog.listener()
     async def on_ready(self):
         Bot = self.Bot
@@ -114,6 +138,7 @@ class BotSys(commands.Cog):
         await Bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=C.ACTIVITY))
         
         logging.info(C.LOG_STR[2].format(Bot.user.name, Bot.user, Bot.user.id, await self.get_localtime()))
+    
     
     ############
     # commands #
@@ -145,10 +170,9 @@ class BotSys(commands.Cog):
         Bot = self.Bot
         
         logging.info(C.LOG_STR[4].format(Bot.user.name, Bot.user, str(Bot.user.id), await self.get_localtime()))
-        
         await ctx.send(C.CTX_STR[1])
-        await ctx.bot.close()
         
+        await ctx.bot.close()
         logging.info(C.LOG_STR[5].format(await self.get_localtime()))
     
     
@@ -177,7 +201,9 @@ class BotSys(commands.Cog):
         Bot = self.Bot
         
         Bot.load_extension(extension)
+        
         logging.warning(C.LOG_STR[8].format(Bot.user.name, usr, usr.id, cog, await self.get_localtime()))
+        
         await ctx.send(C.CTX_STR[3].format(cog))
     
     
@@ -190,16 +216,17 @@ class BotSys(commands.Cog):
         Bot = self.Bot
         
         Bot.unload_extension(extension)
-        logging.warning(C.LOG_STR[9].format(Bot.user.name, usr, usr.id, cog, await self.get_localtime()))
+        
+        logging.warning(C.LOG_STR[9].format(Bot.user.name, usr, usr.id, cog, await self.get_localtime))
         await ctx.send(C.CTX_STR[4].format(Bot.user, cog))
     
     
     @commands.command(name="reload")
     @commands.has_role(C.MODERATOR_ROLE)
     async def reload_cog(self, ctx:commands.Context, cog):
+        """Reloads a bot extension"""
         await self.unload_cog(ctx,cog)
         await self.load_cog(ctx,cog)
 
 def setup(bot):
     bot.add_cog(BotSys(bot))
-
